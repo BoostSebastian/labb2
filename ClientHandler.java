e6a58739de215eca66f397f7efb1e383c52a9cad
BoostSebastian <sebastian@boosterway.com>
2018-02-25 14:13:31 +0100

    last push

--- ClientHandler.java
+++ ClientHandler.java
@@ -11,6 +11,8 @@ public class ClientHandler implements Runnable {
 	private static boolean DEBUGGING = true;
 	private boolean test_500 = false;
 
+	private boolean _302 = false;
+
 	private Socket client;
 
 	// input and output streams
@@ -98,6 +100,7 @@ public class ClientHandler implements Runnable {
 			}
 
 			// Testing a 500 error
+			// CHANGE THIS test_500 to simulate a 500
 			if(test_500){ throw new IOException(); }
 
 
@@ -107,8 +110,14 @@ public class ClientHandler implements Runnable {
 				String path = buildURL(file); 											// build url to obtain file
 				String type = checkTypeOfFile(path); 									// check type of file
 				String ext = getFileExtension(path);									// extension
-				//boolean _root = new File(path).isDirectory();							// check if root directory
 
+				// small fix for handeling htm
+				if(path.contains("html")){
+					// do nothing
+				} else if(path.contains("htm") && !checkURL(file)){
+					path += "l";
+					_302 = true;
+				}
 				
 				if(restrictedDirectory(path) && !checkRoot(path)) {
 
@@ -120,10 +129,15 @@ public class ClientHandler implements Runnable {
 							//   200 RESPONSE  //
 							////////////////////
 
+
 							if (type == "html"){
 	
 								// Get the correct page
-								constructResponseHeader(200, sb, "text/" + ext);		// build header
+								if(_302){
+									constructResponseHeader(302, sb, path);				// build header
+								} else {
+									constructResponseHeader(200, sb, "text/" + ext);	// build header
+								}
 								response.write(sb.toString());							// send header
 								response.write(getData(path));							// send data with html response
 								sb.setLength(0);				 						// reset
@@ -152,9 +166,7 @@ public class ClientHandler implements Runnable {
 
 							/////////////////////////////
 							//     END 200 RESPONSE    //
-							/////////////////////////////
-	
-						
+							////////////////////////////
 	
 						} else { 
 
@@ -210,6 +222,7 @@ public class ClientHandler implements Runnable {
 					// 403 Forbidden
 					constructResponseHeader(403, sb, "");
 					response.write(sb.toString());
+					response.write(_403Temp());
 					sb.setLength(0);
 					response.flush();
 
@@ -285,8 +298,13 @@ public class ClientHandler implements Runnable {
 
 		} else if (responseCode == 302) {
 
+			String link = "https://www.google.se/";
+			if(fileType != ""){
+				link = fileType;
+			}
+
 			sb.append("HTTP/1.1 302 Redirected\r\n");
-			sb.append("Location: https://www.google.se/ \r\n");
+			sb.append("Location: " + link + " \r\n");
 			sb.append("Date:" + getTimeStamp() + "\r\n");
 			sb.append("Server:localhost\r\n");
 			sb.append("\r\n");
@@ -325,7 +343,14 @@ public class ClientHandler implements Runnable {
 			if(i != urlParts.length - 1) my_url.append("/");
 		}
 
-		return my_url.toString();
+		String _url = my_url.toString();
+		if(_url.contains("html")){
+
+		} else if(_url.contains("htm")) {
+			_url += "l";
+		}
+
+		return _url;
 
 	}
 
@@ -377,7 +402,6 @@ public class ClientHandler implements Runnable {
 
 	// Check type of file
 	private static String checkTypeOfFile(String string){
-
 		if(string.toLowerCase().contains("html") || (string.toLowerCase().contains("htm"))){
 			return "html";
 		}
@@ -535,6 +559,84 @@ public class ClientHandler implements Runnable {
 				+			"<hgroup>"
 				+				"<h1>500</h1>"
 				+				"<h2>Ops! Internal Server Error</h2>"
+				+			"</hgroup>"
+				+		"</article>"
+				+	"</div>"
+				+ "</body>"
+				+ "</html>"
+
+				+ "<style>*,"
+				+ "*:after,"
+				+ " *:before {"
+				+ "-webkit-box-sizing: border-box;"
+				+ "-moz-box-sizing: border-box;"
+				+ "-ms-box-sizing: border-box;"
+				+ "box-sizing: border-box;"
+				+ "}"
+				+ "html {background: #ccc;font: bold 14px/20px \"Trajan Pro\", \"Times New Roman\", Times, serif;color: #430400;text-shadow: 0 1px 0 rgba(255, 255, 255, 0.15);}"
+				+ ".error-page-wrap {width: 310px;height: 310px;margin: 155px auto;}"
+				+ ".error-page-wrap:before {box-shadow: 0 0 200px 150px #fff;width: 310px;height: 310px;border-radius: 50%;position: relative;z-index: -1;content: \"\";display: block;}"
+				+ ".error-page {width: 310px;height: 310px;border-radius: 50%;top: -310px;position: relative;text-align: center;background: #d36242;background: -moz-linear-gradient(top, #d36242 0%, darkred 100%);background: -webkit-gradient(linear,left top,left bottom,color-stop(0%, #d36242),color-stop(100%, darkred));background: -webkit-linear-gradient(top, #d36242 0%, darkred 100%);background: -o-linear-gradient(top, #d36242 0%, darkred 100%);background: -ms-linear-gradient(top, #d36242 0%, darkred 100%);background: linear-gradient(to bottom, #d36242 0%, darkred 100%);filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=\"$firstColor\",endColorstr=\"$secondColor\",GradientType=0);}"
+				+ ".error-page:before {width: 63px;height: 63px;border-radius: 50%;box-shadow: 3px 25px 0 5px #c95439;content: \"\";z-index: -1;display: block;position: relative;top: -19px;left: 44px;}"
+				+ ".error-page:after {width: 310px;height: 17px;margin: 0 auto;top: 44px;content: \"\";z-index: -1;display: block;position: relative;background: -moz-radial-gradient(center,ellipse cover,rgba(0, 0, 0, 0.65) 0%,rgba(35, 26, 26, 0) 59%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: -webkit-gradient(radial,center center,0px,center center,100%,color-stop(0%, rgba(0, 0, 0, 0.65)),color-stop(59%, rgba(35, 26, 26, 0)),color-stop(100%, rgba(60, 44, 44, 0)));"
+				+ "background: -webkit-radial-gradient(center,ellipse cover,rgba(0, 0, 0, 0.65) 0%,rgba(35, 26, 26, 0) 59%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: -o-radial-gradient(center,ellipse cover,rgba(0, 0, 0, 0.65) 0%,rgba(35, 26, 26, 0) 59%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: -ms-radial-gradient(center,ellipse cover,rgba(0, 0, 0, 0.65) 0%,rgba(35, 26, 26, 0) 59%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: radial-gradient(ellipse at center,rgba(0, 0, 0, 0.65) 0%,rgba(35, 26, 26, 0) 59%,rgba(60, 44, 44, 0) 100%);"
+				+ "filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=\"#a6000000\",endColorstr=\"#003c2c2c\",GradientType=1);}"
+				+ ".error-page h1 {color: rgba(255, 255, 255, 0.94);font-size: 100px;margin: 65px auto 0 auto;text-shadow: 0px 0 7px rgba(0, 0, 0, 0.5);}"
+				+ ".error-page h1:before {width: 260px;height: 1px;position: relative;margin: 0 auto;top: 70px;content: \"\";display: block;"
+				+ "background: -moz-radial-gradient(center,ellipse cover,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: -webkit-gradient(radial,center center,0px,center center,100%,color-stop(0%, rgba(111, 25, 25, 0.65)),color-stop(70%, rgba(75, 38, 38, 0)),color-stop(100%, rgba(60, 44, 44, 0)));background: -webkit-radial-gradient(center,ellipse cover,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: -o-radial-gradient(center,ellipse cover,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: -ms-radial-gradient(center,ellipse cover,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: radial-gradient(ellipse at center,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);"
+				+ "filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=\"#a66f1919\",endColorstr=\"#003c2c2c\",GradientType=1);}"
+				+ ".error-page h1:after {width: 260px;height: 1px;content: \"\";display: block;opacity: 0.2;margin: 0 auto;top: 50px;position: relative;background: -moz-radial-gradient(center,ellipse cover,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);"
+				+ "background: -webkit-gradient(radial,center center,0px,center center,100%,color-stop(0%, rgba(247, 173, 148, 0.65)),color-stop(99%, rgba(255, 255, 255, 0.01)),color-stop(100%, rgba(255, 255, 255, 0)));"
+				+ "background: -webkit-radial-gradient(center,ellipse cover,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);"
+				+ "background: -o-radial-gradient(center,ellipse cover,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);"
+				+ "background: -ms-radial-gradient(center,	ellipse cover,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);"
+				+ "background: radial-gradient(ellipse at center,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=\"#a6f7ad94\",endColorstr=\"#00ffffff\",GradientType=1);}"
+				+ ".error-page h2 {margin: 55px 0 30px 0;font-size: 17px;}"
+				+ ".error-page h2:before {width: 130px;height: 1px;position: relative;margin: 0 auto;top: 31px;content: \"\";display: block;"
+				+ "background: -moz-radial-gradient(center,ellipse cover,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: -webkit-gradient(radial,center center,0px,center center,100%,color-stop(0%, rgba(111, 25, 25, 0.65)),color-stop(70%, rgba(75, 38, 38, 0)),color-stop(100%, rgba(60, 44, 44, 0)));"
+				+ "background: -webkit-radial-gradient(center,ellipse cover,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);background: -o-radial-gradient(center,ellipse cover,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: -ms-radial-gradient(center,ellipse cover,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);"
+				+ "background: radial-gradient(ellipse at center,rgba(111, 25, 25, 0.65) 0%,rgba(75, 38, 38, 0) 70%,rgba(60, 44, 44, 0) 100%);"
+				+ "filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=\"#a66f1919\",endColorstr=\"#003c2c2c\",GradientType=1);}"
+				+ ".error-page h2:after {width: 130px;height: 1px;content: \"\";display: block;opacity: 0.2;margin: 0 auto;top: 11px;position: relative;background: -moz-radial-gradient(center,ellipse cover,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);"
+				+ "background: -webkit-gradient(radial,center center,0px,center center,100%,color-stop(0%, rgba(247, 173, 148, 0.65)),color-stop(99%, rgba(255, 255, 255, 0.01)),color-stop(100%, rgba(255, 255, 255, 0)));"
+				+ "background: -webkit-radial-gradient(center,ellipse cover,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);background: -o-radial-gradient(center,ellipse cover,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);"
+				+ "background: -ms-radial-gradient(center,ellipse cover,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);background: radial-gradient(ellipse at center,rgba(247, 173, 148, 0.65) 0%,rgba(255, 255, 255, 0.01) 99%,rgba(255, 255, 255, 0) 100%);"
+				+ "filter: progid:DXImageTransform.Microsoft.gradient(startColorstr=\"#a6f7ad94\",endColorstr=\"#00ffffff\",GradientType=1);}"
+				+ ".error-back {text-decoration: none;color: #430400;font-size: 15px;}"
+				+ ".error-back:hover {color: #eb957d;text-shadow: 0 0 3px black;}</style>";
+
+
+				return html;
+		
+	}
+
+	// 500 Error
+	private String _403Temp(){
+
+		String html = ""
+				+ "<html>"
+				+ "<head>"
+				+	"<meta charset=\"utf-8\">"
+				+	"<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">"
+				+		"<title>404 Page no found</title>"
+				+		"<meta name=\"viewport\" content=\"width=device-width\">"
+				+ "</head>"
+				+ "<body>"
+				+	"<div class=\"error-page-wrap\">"
+				+		"<article class=\"error-page gradient\">"
+				+			"<hgroup>"
+				+				"<h1>403</h1>"
+				+				"<h2>Ops! Forbidden</h2>"
 				+			"</hgroup>"
 				+		"</article>"
 				+	"</div>"
